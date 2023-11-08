cff6cbab557d14a88be46d802a1c520cb643148c
heeraraiyan <heeraraiyan07@yahoo.com>
2023-11-08 17:53:11 +0600

    footer section completed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import Banner from "../components/Banner";
 import Categories from "../components/Categories";
+import Footer from "../components/Footer";
 import Latest from "../components/Latest";
 import Navbar from "../components/Navbar";
 import WhyUs from "../components/WhyUs";
@@ -10,7 +11,7 @@ import WhyUs from "../components/WhyUs";
 const Home = () => {
     return (
         <div>
-            <div className="bg-gradient-to-r from-cyan-400 to-sky-800">
+            <div className="bg-gradient-to-r from-cyan-100 to-sky-300">
             <Navbar></Navbar>
             <Banner></Banner>
             
@@ -21,6 +22,7 @@ const Home = () => {
         <Categories></Categories>
         <WhyUs></WhyUs>
         <Latest></Latest>
+        <Footer></Footer>
 
         
         </div>
